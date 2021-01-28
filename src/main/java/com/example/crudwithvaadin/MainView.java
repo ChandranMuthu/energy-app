1245a1be38147051cdec42b4cc94b01d62d9bd56
Chandran M <m.chandran200@gmail.com>
2021-01-28 11:27:22 +0530

    vaadin chart prototype

--- src/main/java/com/example/crudwithvaadin/MainView.java
+++ src/main/java/com/example/crudwithvaadin/MainView.java
@@ -1,8 +1,15 @@
 package com.example.crudwithvaadin;
 
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.*;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.contextmenu.SubMenu;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -33,6 +40,65 @@ public class MainView extends VerticalLayout {
 
 		// build layout
 		HorizontalLayout actions = new HorizontalLayout(filter);
+
+		MenuBar menuBar = new MenuBar();
+		Text selected = new Text("");
+		Div message = new Div(new Text("Selected: "), selected);
+
+		MenuItem project = menuBar.addItem("Home");
+		MenuItem account = menuBar.addItem("Account");
+		menuBar.addItem("Sign Out", e -> selected.setText("Sign Out"));
+
+		SubMenu projectSubMenu = project.getSubMenu();
+		MenuItem users = projectSubMenu.addItem("Users");
+		MenuItem billing = projectSubMenu.addItem("Billing");
+
+		SubMenu usersSubMenu = users.getSubMenu();
+		usersSubMenu.addItem("List", e -> selected.setText("List"));
+		usersSubMenu.addItem("Add", e -> selected.setText("Add"));
+
+		SubMenu billingSubMenu = billing.getSubMenu();
+		billingSubMenu.addItem("Invoices", e -> selected.setText("Invoices"));
+		billingSubMenu.addItem("Balance Events",
+				e -> selected.setText("Balance Events"));
+
+		account.getSubMenu().addItem("Edit Profile",
+				e -> selected.setText("Edit Profile"));
+		account.getSubMenu().addItem("Privacy Settings",
+				e -> selected.setText("Privacy Settings"));
+		add(menuBar, message);
+
+
+		Chart chart = new Chart();
+
+		Configuration configuration = chart.getConfiguration();
+		configuration.setTitle("Monthly Average Reading");
+		configuration.setSubTitle("Source: Some Device");
+		chart.getConfiguration().getChart().setType(ChartType.COLUMN);
+
+		configuration.addSeries(new ListSeries("Device 1", 49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4));
+		configuration.addSeries(new ListSeries("Device 2", 83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3));
+		configuration.addSeries(new ListSeries("Device 3", 48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2));
+		configuration.addSeries(new ListSeries("Device 4", 42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1));
+
+		XAxis x = new XAxis();
+		x.setCrosshair(new Crosshair());
+		x.setCategories("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
+				"Sep", "Oct", "Nov", "Dec");
+		configuration.addxAxis(x);
+
+		YAxis y = new YAxis();
+		y.setMin(0);
+		y.setTitle("Voltage used (volt)");
+		configuration.addyAxis(y);
+
+		Tooltip tooltip = new Tooltip();
+		tooltip.setShared(true);
+		configuration.setTooltip(tooltip);
+
+		add(chart);
+
+
 		add(actions, grid);
 
 		grid.setHeight("300px");
